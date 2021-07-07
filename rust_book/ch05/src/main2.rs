bc8f8694bb8cb193ad64da2a2e43bc651dffa31c
urawa72 <urawa72@gmail.com>
2021-07-07 15:10:37 +0900

    update ch05

--- rust_book/ch05/src/main2.rs
+++ rust_book/ch05/src/main2.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("{}", "hello, world");
+    let rec1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rec1)
+    );
+    println!("rec1 is {:?}", rec1);
+    println!("rec1 is {:#?}", rec1);
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area(rec: &Rectangle) -> u32 {
+    rec.width * rec.height
 }
