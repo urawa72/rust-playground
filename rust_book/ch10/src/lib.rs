839cef4d92c52fa8068fc5cac03e3a17a2503c20
urawa72 <urawa72@gmail.com>
2021-07-11 21:54:48 +0900

    update ch10

--- rust_book/ch10/src/lib.rs
+++ rust_book/ch10/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub trait Summary {
     fn summarize(&self) -> String;
 }
@@ -25,5 +27,30 @@ pub struct Tweet {
 impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
+    }
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    pub fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl <T: Display + PartialOrd> Pair<T> {
+    pub fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        }else{
+            println!("The largest member is y = {}", self.y);
+        }
     }
 }
--- rust_book/ch10/src/main.rs
+++ rust_book/ch10/src/main.rs
@@ -1,33 +1,48 @@
 mod lib;
-use lib::Tweet;
-
-use crate::lib::Summary;
+use crate::lib::{notify, Summary};
+use lib::{Pair, Tweet};
 
 fn main() {
-    // check_largest();
+    check_largest();
     struct_generics();
     method_generics();
     trait_sample();
+    let r = return_summarize();
+    notify(&r);
+    trait_boundary();
 }
 
-// fn check_largest() {
-//     let number_list = vec![34, 50, 25, 100, 65];
-//     let result = largest(&number_list);
-//     println!("The largest number is {}", result);
-//     let char_list = vec!['y', 'm', 'a', 'q'];
-//     let result = largest(&char_list);
-//     println!("The larget{}", result);
-// }
+fn check_largest() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The larget{}", result);
+    let number_list = vec![34, 50, 25, 100, 65, 300, 45];
+    let result = largest_copy(&number_list);
+    println!("The largest number is {}", result);
+}
 
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
-//     for &item in list.iter() {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn largest_copy<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
 
 struct Point<T> {
     x: T,
@@ -86,4 +101,19 @@ fn trait_sample() {
         retweet: false,
     };
     println!("1 new tweet: {}", tweet.summarize());
+    notify(&tweet)
+}
+
+fn return_summarize() -> impl Summary {
+    Tweet {
+        username: "horse_ebooks".to_string(),
+        content: "of course, as you probably already know, people".to_string(),
+        reply: false,
+        retweet: false,
+    }
+}
+
+fn trait_boundary() {
+    let p = Pair::new(4, 10);
+    p.cmp_display();
 }
