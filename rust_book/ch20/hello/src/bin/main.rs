a8121fdf9a40724afbd0448c57cde039a394c9a3
urawa72 <urawa72@gmail.com>
2021-07-22 14:36:23 +0900

    update ch20

--- rust_book/ch20/hello/src/bin/main.rs
+++ rust_book/ch20/hello/src/bin/main.rs
@@ -1,15 +1,26 @@
+extern crate hello;
+use hello::ThreadPool;
 use std::{
     fs::File,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() {
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        handle_connection(stream)
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -17,8 +28,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
