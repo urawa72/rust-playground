c265d6a078f8eae1426a58843de39e783f81edbd
urawa72 <urawa72@gmail.com>
2021-07-28 09:22:07 +0900

    delete files

--- graphql_sample/migrations/2021-07-27-121645_create_photos/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-CREATE TABLE photos (
-  id SERIAL NOT NULL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  description TEXT,
-  created_at timestamp with time zone NOT NULL default CURRENT_TIMESTAMP,
-  updated_at timestamp with time zone NOT NULL default CURRENT_TIMESTAMP
-);
--- graphql_sample/src/schema.rs
+++
@@ -1,9 +0,0 @@
-table! {
-    photos (id) {
-        id -> Int4,
-        name -> Varchar,
-        description -> Nullable<Text>,
-        created_at -> Timestamptz,
-        updated_at -> Timestamptz,
-    }
-}
