4f4f085661bdac26ed18deb836645a320f08de6a
urawa72 <urawa72@gmail.com>
2021-08-07 13:11:36 +0900

    bitonic-sorter: add second sort

--- rust_entry_book/ch03/bitonic-sorter/src/first.rs
+++ rust_entry_book/ch03/bitonic-sorter/src/first.rs
@@ -31,14 +31,14 @@ mod tests {
 
     #[test]
     fn sort_u32_ascending() {
-        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
         sort(&mut x, true);
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
 
     #[test]
     fn sort_u32_descending() {
-        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
         sort(&mut x, false);
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
