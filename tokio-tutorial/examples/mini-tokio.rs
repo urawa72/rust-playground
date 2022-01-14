3b5fa3475214db8ce92d7c272ed7c78e84b35c76
urawa72 <urawa72@gmail.com>
2022-01-14 22:14:02 +0900

    tokio-tutorial: mini-tokio(done)

--- tokio-tutorial/examples/mini-tokio.rs
+++ tokio-tutorial/examples/mini-tokio.rs
@@ -1,24 +1,33 @@
 use crossbeam::channel;
 use futures::task::{self, ArcWake};
 use std::{
-    collections::VecDeque,
+    cell::RefCell,
     future::Future,
     pin::Pin,
     sync::{Arc, Mutex},
-    task::Context,
+    task::{Context, Waker},
     time::{Duration, Instant},
 };
 use std::{task::Poll, thread};
 
 fn main() {
-    let mut mini_tokio = MiniTokio::new();
+    let mini_tokio = MiniTokio::new();
 
     mini_tokio.spawn(async {
-        let when = Instant::now() + Duration::from_millis(10);
-        let future = Delay { when };
-        let out = future.await;
-        assert_eq!(out, "done")
-    })
+        spawn(async {
+            delay(Duration::from_millis(100)).await;
+            println!("world")
+        });
+
+        spawn(async {
+            println!("hello");
+        });
+
+        delay(Duration::from_millis(200)).await;
+        std::process::exit(0);
+    });
+
+    mini_tokio.run();
 }
 
 struct MiniTokio {
@@ -26,17 +35,6 @@ struct MiniTokio {
     sender: channel::Sender<Arc<Task>>,
 }
 
-struct Task {
-    future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
-    executor: channel::Sender<Arc<Task>>,
-}
-
-
-impl ArcWake for Task {
-    fn wake_by_ref(arc_self: &Arc<Self>) {
-        let _ = arc_self.executor.send(arc_self.clone());
-    }
-}
 
 impl MiniTokio {
     fn new() -> MiniTokio {
@@ -53,9 +51,86 @@ impl MiniTokio {
     }
 
     fn run(&self) {
+        CURRENT.with(|cell| {
+            *cell.borrow_mut() = Some(self.sender.clone());
+        });
+
         while let Ok(task) = self.scheduled.recv() {
             task.poll();
         }
+    }
+}
+
+pub fn spawn<F>(future: F)
+where
+    F: Future<Output = ()> + Send + 'static,
+{
+    CURRENT.with(|cell| {
+        let borrow = cell.borrow();
+        let sender = borrow.as_ref().unwrap();
+        Task::spawn(future, sender);
+    });
+}
+
+thread_local! {
+    static CURRENT: RefCell<Option<channel::Sender<Arc<Task>>>> = RefCell::new(None);
+}
+
+async fn delay(dur: Duration) {
+    struct Delay {
+        when: Instant,
+        waker: Option<Arc<Mutex<Waker>>>,
+    }
+
+    impl Future for Delay {
+        type Output = ();
+
+        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+            if let Some(waker) = &self.waker {
+                let mut waker = waker.lock().unwrap();
+                if !waker.will_wake(cx.waker()) {
+                    *waker = cx.waker().clone();
+                }
+            } else {
+                let when = self.when;
+                let waker = Arc::new(Mutex::new(cx.waker().clone()));
+                self.waker = Some(waker.clone());
+
+                thread::spawn(move || {
+                    let now = Instant::now();
+                    if now < when {
+                        thread::sleep(when - now);
+                    }
+
+                    let waker = waker.lock().unwrap();
+                    waker.wake_by_ref();
+                });
+            }
+
+            if Instant::now() >= self.when {
+                Poll::Ready(())
+            } else {
+                Poll::Pending
+            }
+        }
+    }
+
+    let future = Delay {
+        when: Instant::now() + dur,
+        waker: None,
+    };
+
+    future.await;
+}
+
+struct Task {
+    future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    executor: channel::Sender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let _ = arc_self.executor.send(arc_self.clone());
     }
 }
 
@@ -79,31 +154,4 @@ impl Task {
     }
 }
 
-struct Delay {
-    when: Instant,
-}
 
-impl Future for Delay {
-    type Output = &'static str;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<&'static str> {
-        if Instant::now() >= self.when {
-            println!("Hello world");
-            Poll::Ready("done")
-        } else {
-            let waker = cx.waker().clone();
-            let when = self.when;
-            thread::spawn(move || {
-                let now = Instant::now();
-                if now < when {
-                    thread::sleep(when - now);
-                }
-                // 続きの処理を行うため紐づいているタスクを実行予約するよう実行器へ合図が送られる
-                // ポーリングによって処理を次に進めることができる準備が整ったということを実行器に通知できるようになったときに
-                // リソースはwake()を呼び出す
-                waker.wake();
-            });
-            Poll::Pending
-        }
-    }
-}
