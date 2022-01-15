8da1d82fe55be66e97b53de14b4cc56641305c5f
urawa72 <urawa72@gmail.com>
2022-01-15 15:22:06 +0900

    tokio-tutorial: streams

---
+++ tokio-tutorial/examples/streams.rs
@@ -0,0 +1,35 @@
+use futures::StreamExt;
+use mini_redis::client;
+
+async fn publish() -> mini_redis::Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    client.publish("numbers", "1".into()).await?;
+    client.publish("numbers", "two".into()).await?;
+    client.publish("numbers", "3".into()).await?;
+    client.publish("numbers", "four".into()).await?;
+    client.publish("numbers", "five".into()).await?;
+    client.publish("numbers", "6".into()).await?;
+    Ok(())
+}
+
+async fn subscribe() -> mini_redis::Result<()> {
+    let client = client::connect("127.0.0.1:6379").await?;
+    let subscriber = client.subscribe(vec!["numbers".to_string()]).await?;
+    let messages = subscriber.into_stream();
+    // streamをpinする
+    // pinされている = メモリ上でそれ以上ムーブされることがない
+    // 呼び出し側はそのポインタが有効であることを確信できる
+    tokio::pin!(messages);
+    while let Some(msg) = messages.next().await {
+        println!("got = {:?}", msg);
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> mini_redis::Result<()> {
+    tokio::spawn(async { publish().await });
+    subscribe().await?;
+    println!("DONE");
+    Ok(())
+}
