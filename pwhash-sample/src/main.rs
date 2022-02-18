eb6d8ff45874440b6438f800999b380f4e19fc92
urawa72 <urawa72@gmail.com>
2022-02-18 14:03:41 +0900

    add pwhash sample

---
+++ pwhash-sample/src/main.rs
@@ -0,0 +1,17 @@
+use pwhash::bcrypt;
+use pwhash::bcrypt::{BcryptSetup, BcryptVariant};
+
+fn main() {
+    let password = "test_password";
+    let hashed1 = bcrypt::hash_with(
+        BcryptSetup {
+            salt: Some("salt"),
+            variant: Some(BcryptVariant::V2y),
+            ..Default::default()
+        },
+        password,
+    ).unwrap();
+    let hashed2 = bcrypt::hash(password).unwrap();
+    assert!(bcrypt::verify("test_password", &hashed1));
+    assert!(bcrypt::verify("test_password", &hashed2));
+}
