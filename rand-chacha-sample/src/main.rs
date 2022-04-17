aaabf3bd32b14a57f87c92b7d634950ec8c6a547
urawa72 <urawa72@gmail.com>
2022-04-18 08:29:49 +0900

    update rand-chacha-sample

--- rand-chacha-sample/src/main.rs
+++ rand-chacha-sample/src/main.rs
@@ -7,12 +7,16 @@ fn main() {
     let mut csp_rng = ChaCha20Rng::from_entropy();
     let mut data = [0u8; 32];
     csp_rng.fill_bytes(&mut data);
+    println!("raw_binary = {:?}", data);
+
+    let mut hasher256 = Sha256::new();
+    hasher256.update(data);
+    let hashed: String = format!("{:x}", hasher256.finalize());
+    println!("hashed = {}", hashed);
 
     let base64_encoded = base64::encode(data);
     println!("base64_encoded = {}", base64_encoded);
 
-    let mut hasher256 = Sha256::new();
-    hasher256.update(base64_encoded.as_bytes());
-    let hashed: String = format!("{:x}", hasher256.finalize());
-    println!("hashed = {}", hashed);
+    let base64_decoded = base64::decode(base64_encoded).unwrap();
+    println!("base64_decoded = {:?}", base64_decoded);
 }
